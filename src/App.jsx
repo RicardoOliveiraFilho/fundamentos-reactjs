ae51fad29878a30a22937894806a5b03d3a67b59
RicardoOliveiraFilho <ricardo.oliveira1307@gmail.com>
2022-06-28 21:35:16 -0300

    Fundamentos ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <div>
+      <Post author="Michelle Thaynara" content="Conteúdo do post de Michelle!!!" />
+      <Post author="Ricardo Oliveira" content="Conteúdo do post de Ricardo!!!" />
+      <Post author="Desconhecido" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga officiis odio sed mollitia aperiam iure tenetur nobis aliquam, odit culpa. Tempore, natus accusamus facere cupiditate iure porro! Iusto, accusamus adipisci." />
+    </div>
+  )
+}
