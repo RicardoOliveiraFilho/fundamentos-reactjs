e2039b84cc7d2a89824aa7133058a452bf622b1d
RicardoOliveiraFilho <ricardo.oliveira1307@gmail.com>
2022-06-28 22:00:51 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post"
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post author="Michelle Thaynara" content="Conteúdo do post de Michelle!!!" />
       <Post author="Ricardo Oliveira" content="Conteúdo do post de Ricardo!!!" />
       <Post author="Desconhecido" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga officiis odio sed mollitia aperiam iure tenetur nobis aliquam, odit culpa. Tempore, natus accusamus facere cupiditate iure porro! Iusto, accusamus adipisci." />
